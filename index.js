ca1c90c5a5506169d2cd0b0a60c4334f6db5fcad
ximenanavarro <ximenanavarro@Ximenas-MacBook-Pro.local>
2022-11-04 15:36:10 -0600

    commit tree e4caecd599e2515cf7e0f6dfe3d5e8b24f2232fc

--- index.js
+++ index.js
@@ -1,1 +1,32 @@
 // Write your solution here!
+const cats = ["Milo", "Otis", "Garfield"];
+
+function destructivelyAppendCat(name) {
+    cats.push(name)
+}
+
+function destructivelyPrependCat(name) {
+    cats.unshift(name)
+}
+
+function destructivelyRemoveLastCat() {
+    cats.pop()
+}
+function destructivelyRemoveFirstCat() {
+    cats.shift()
+}
+function appendCat(name) {
+    return [...cats, name]
+   
+}
+function prependCat(name) {
+    return [name, ...cats]
+}
+function removeLastCat() {
+    const newCats = [...cats]
+    return newCats.slice(0, 2)
+}
+function removeFirstCat() {
+    const newCatss = [...cats]
+    return newCatss.slice(1)
+}
